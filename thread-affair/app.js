5d7e40a0173c81e383072a4cedaa6b525b741ac1
Dragos Iordache <dragos.i@adoreme.com>
2021-09-14 21:59:35 +0300

    10:00

--- thread-affair/app.js
+++ thread-affair/app.js
@@ -1,6 +1,68 @@
 class NewsletterForm extends React.Component {
+  // v1
+  state = {
+    email: '',
+    inputMessage: '',
+  };
+
+  validateEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  // handleSubmit
+  onSubmit = (event) => {
+    event.preventDefault();
+
+    const email = this.state.email;
+
+    if (!this.validateEmail(email)) {
+      this.setState({
+        inputMessage: 'Please use a valid email',
+      });
+
+      return;
+    }
+
+    setTimeout(() => {
+      alert(`Hello, ${email} thank you for subscribing`);
+    }, 3000);
+  };
+
+  onInputChange = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+
   render() {
-    return <h1>hello world</h1>;
+    return (
+      <form onSubmit={this.onSubmit} className="form-newsletter container">
+        <label htmlFor="field-newsletter">
+          Subscribe to our <span>newsletter</span>
+        </label>
+
+        <div>
+          <input
+            type="text"
+            name="field-newsletter"
+            id="field-newsletter"
+            value={this.state.email}
+            onChange={this.onInputChange}
+            placeholder="enter your email address to receive the latest news!"
+          ></input>
+
+          {this.state.inputMessage.length > 0 ? (
+            <div className="message">{this.state.inputMessage}</div>
+          ) : null}
+        </div>
+
+        <button type="submit" title="Subscribe">
+          Subscribe
+        </button>
+      </form>
+    );
   }
 }
 
