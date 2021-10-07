530a784dcb75a09b0001c18956de5a09587ea31f
Dragos Iordache <dragos.i@adoreme.com>
2021-10-07 20:16:04 +0300

    8:16

--- react-components/ex02/app.js
+++ react-components/ex02/app.js
@@ -1,8 +1,9 @@
 const person = {
   name: 'Dragos',
   surname: 'Iordache',
-  age: 32,
+  age: 34,
   petOwner: false,
+  // html,javascript,css
   skills: [
     'html',
     'javascript',
@@ -31,3 +32,44 @@ const person = {
     },
   ],
 };
+
+console.warn(`
+  Folosind obiectul person si reduce,
+  afiseaza in consola un string care contine
+  skillurile de pe pozitiile pare ale arrayului, separate prin virgula
+`);
+let message = '';
+
+message = person.skills.reduce((message, skill, index) => {
+  if (index % 2 !== 0) {
+    return message;
+  }
+
+  return `${message}${index === 0 ? '' : ','}${skill}`;
+}, '');
+console.log(message);
+
+console.warn(`
+  In mod similar, afiseaza skillurile care NU incep cu j.
+`);
+message = person.skills.reduce((message, skill, index) => {
+  if (skill.startsWith('j')) {
+    return message;
+  }
+
+  return `${message}${index === 0 ? '' : ','}${skill}`;
+}, '');
+console.log(message);
+
+console.warn(`
+  Folosind reduce afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy, xxx yyy."
+`);
+message = person.friends.reduce(
+  (message, { name, surname }, index, friends) => {
+    const punctuation = index === friends.length - 1 ? '.' : ', ';
+
+    return `${message}${name} ${surname}${punctuation}`;
+  },
+  'Prietenii mei se numesc ',
+);
+console.log(message);
