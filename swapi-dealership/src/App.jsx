caed3c03e71fd1a55a99af144310859f194b24cb
Dragos Iordache <dragos.i@adoreme.com>
2021-10-14 22:10:19 +0300

    10:)0

---
+++ swapi-dealership/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <header className="navbar-dark fixed-top bg-dark border-bottom border-warning">
+        <nav className="container d-flex justify-content-between">
+          <h1 className="display-6 text-warning">Swapi Vehicles</h1>
+
+          {/* search */}
+        </nav>
+      </header>
+
+      <main className="container mb-4">Content</main>
+
+      <footer className="container mb-4">FOoter</footer>
+    </>
+  );
+}
+
+export default App;
