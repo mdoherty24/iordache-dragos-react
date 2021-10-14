04d919ffe4ea5567a3c76895e215ff760d48c4d7
Dragos Iordache <dragos.i@adoreme.com>
2021-10-14 20:17:08 +0300

    swapi ready

---
+++ swapi-cinema/src/components/MetaImage.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+const baseUrl =
+  'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI';
+const apiKey = process.env.REACT_APP_WEB_SEARCH_KEY;
+const host = 'contextualwebsearch-websearch-v1.p.rapidapi.com';
+
+export const MetaImage = ({ term }) => {
+  const [busy, setBusy] = useState(true);
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const refinedSearchTerm = encodeURIComponent(`star wars ${term}`);
+    const random = Math.floor(Math.random() * 10000) + 2;
+
+    setTimeout(() => {
+      fetch(
+        `${baseUrl}?q=${refinedSearchTerm}&pageNumber=1&pageSize=1&autoCorrect=true`,
+        {
+          headers: {
+            'x-rapidapi-key': apiKey,
+            'x-rapidapi-host': host,
+            useQueryString: true,
+          },
+        },
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const imageUrl = data.value[0].url;
+
+          setBusy(false);
+          setImageUrl(imageUrl);
+        });
+    }, random);
+  }, []);
+
+  return (
+    <>
+      {busy === true ? (
+        '...loading'
+      ) : (
+        <img className="img-fluid" src={imageUrl} alt={term}></img>
+      )}
+    </>
+  );
+};
+
+export default MetaImage;
