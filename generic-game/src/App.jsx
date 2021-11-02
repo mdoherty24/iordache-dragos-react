9898e460e7a7af145e70d2d5dacf4d91688763c9
Dragos Iordache <dragos.i@adoreme.com>
2021-11-02 21:49:43 +0200

    9:49

--- generic-game/src/App.jsx
+++ generic-game/src/App.jsx
@@ -1,13 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { clickClicker } from './actions/creators/ui';
 import { Footer, Header } from './components/common';
 // import { STH, STH } from './actions/types/ui';
 // import {dispatchState} from './actions/creators/ui';
 
 export const App = () => {
+  const clicker = useSelector((state) => {
+    const { ui } = state;
+
+    return ui.clicker;
+  });
+  const dispatch = useDispatch();
+
   return (
     <>
       <Header></Header>
       <main>
-        <button onClik={() => {}}>Test</button>
+        <div>Value is: {clicker}</div>
+        <button
+          onClick={() => {
+            dispatch(clickClicker());
+          }}
+        >
+          Test
+        </button>
       </main>
       <Footer></Footer>
     </>
--- generic-game/src/reducers/ui.js
+++ generic-game/src/reducers/ui.js
@@ -1,9 +1,19 @@
+import { CLICKER_CLICK } from '../actions/types/ui';
+
 const initialState = {
   clicker: 0,
 };
 
 export const uiReducer = (state = initialState, { type, payload }) => {
-  return state;
+  switch (type) {
+    case CLICKER_CLICK:
+      const newState = {};
+
+      newState.clicker = state.clicker + payload;
+      return newState;
+    default:
+      return state;
+  }
 };
 
 export default uiReducer;
