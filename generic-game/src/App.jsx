f387814367f52a46075877dcc1d1b475a1613a73
Dragos Iordache <dragos.i@adoreme.com>
2021-11-02 20:33:02 +0200

    8:32

---
+++ generic-game/src/App.jsx
@@ -0,0 +1,20 @@
+import { Footer, Header } from './components/common';
+// import { STH, STH } from './actions/types/ui';
+// import {dispatchState} from './actions/creators/ui';
+
+export const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <button onClik={() => {}}>Test</button>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
+
+// actions -> {type: '', payload: {}}  /types /creators
+// reducers
---
+++ generic-game/src/reducers/ui.js
@@ -0,0 +1,9 @@
+const initialState = {
+  clicker: 0,
+};
+
+export const uiReducer = (state = initialState, { type, payload }) => {
+  return state;
+};
+
+export default uiReducer;
