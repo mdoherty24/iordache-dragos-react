64a5a2546e1d225dc0c32263c9550fa5ce64a5b2
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-11-04 20:43:52 +0200

    8:45

---
+++ generic-game/src/api/googleAuth.js
@@ -0,0 +1,41 @@
+let eventBound = false;
+
+// recipe
+export const initializeGoogleAuth = async () => {
+  return new Promise((resolve) => {
+    gapi.load('client:auth2', () => {
+      gapi.client
+        .init({
+          clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          scope: 'email profile',
+        })
+        .then(() => {
+          const GoogleAuth = gapi.auth2.getAuthInstance();
+          const googleUser = GoogleAuth.currentUser.get();
+
+          if (!eventBound) {
+            GoogleAuth.isSignedIn.listen((isAuthenticated) => {
+              authenticationChangeHandler(isAuthenticated, googleUser);
+            });
+
+            authenticationChangeHandler(
+              GoogleAuth.isSignedIn.get(),
+              googleUser,
+            );
+
+            eventBound = true;
+          }
+
+          resolve(GoogleAuth);
+        });
+    });
+  });
+};
+
+const authenticationChangeHandler = (isAuthenticated, googleUser) => {
+  if (isAuthenticated) {
+    console.log('logged in', googleUser);
+  } else {
+    console.log('not logged in');
+  }
+};
