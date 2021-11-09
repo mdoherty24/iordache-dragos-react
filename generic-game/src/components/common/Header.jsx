330e5c8ea3554ac089ad7bb477d08e83ef8fda6a
Dragos Iordache <dragos.i@adoreme.com>
2021-11-09 22:03:33 +0200

    10:03

--- generic-game/src/components/common/Header.jsx
+++ generic-game/src/components/common/Header.jsx
@@ -1,5 +1,33 @@
+import { SiLetterboxd } from 'react-icons/si';
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
-  return 'header';
+  const renderUserControls = () => {
+    const authenticated = true;
+
+    if (authenticated) {
+      return 'user is logged in';
+    } else {
+      return 'user is not logged in';
+    }
+  };
+
+  return (
+    <header className="shadow p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <header>
+          <h1 className="uppercase text-lg font-bold">
+            <Link to="/" title="Go home" className="flex items-center">
+              <SiLetterboxd className="mr-2"></SiLetterboxd>
+              Word Game
+            </Link>
+          </h1>
+        </header>
+
+        {renderUserControls()}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
