97df5a6fccea79a5083a7ec6dc0faaed72128591
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-11-25 21:01:59 +0200

    9:01

--- generic-game/src/components/profile/Creature.jsx
+++ generic-game/src/components/profile/Creature.jsx
@@ -1,27 +1,74 @@
+import { useSelector } from 'react-redux';
 import './Creature.css';
 
 export const Creature = () => {
+  const { mainColor, secondaryColor, eyeColor } = useSelector(({ profile }) => {
+    return profile.creature;
+  });
+
   return (
-    <div className="creature">
+    <div
+      className="creature"
+      style={{
+        backgroundColor: mainColor,
+        borderColor: secondaryColor,
+      }}
+    >
       <div className="creature__eyes">
         <div className="eye eye--blinking creature__eye creature__eye--left">
-          <div className="eye__iris"></div>
+          <div
+            className="eye__iris"
+            style={{
+              backgroundColor: eyeColor,
+            }}
+          ></div>
 
-          <div className="eye__lid"></div>
+          <div
+            className="eye__lid"
+            style={{
+              backgroundColor: secondaryColor,
+            }}
+          ></div>
         </div>
 
         <div className="eye eye--blinking creature__eye creature__eye--right">
-          <div className="eye__iris"></div>
+          <div
+            className="eye__iris"
+            style={{
+              backgroundColor: eyeColor,
+            }}
+          ></div>
 
-          <div className="eye__lid"></div>
+          <div
+            className="eye__lid"
+            style={{
+              backgroundColor: secondaryColor,
+            }}
+          ></div>
         </div>
       </div>
 
       <div className="creature__nose-line">
-        <div className="nose creature__nose">
-          <div className="nose__nostril nose__nostril--left"></div>
+        <div
+          className="nose creature__nose"
+          style={{
+            backgroundColor: secondaryColor,
+            borderColor: mainColor,
+          }}
+        >
+          <div
+            className="nose__nostril nose__nostril--left"
+            style={{
+              backgroundColor: mainColor,
+            }}
+          ></div>
 
-          <div className="nose__nostril nose__nostril--right"></div>
+          <div
+            className="nose__nostril nose__nostril--right"
+            style={{
+              backgroundColor: mainColor,
+            }}
+          ></div>
         </div>
       </div>
     </div>
