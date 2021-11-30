b1fc80aba09a4f30e71dce07a7a2cbc5082a8467
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-11-30 20:35:27 +0200

    8935

--- generic-game/src/pages/GamePage.jsx
+++ generic-game/src/pages/GamePage.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Authorize } from '../components/auth';
 import { Creature } from '../components/profile';
 import { Button } from '../components/ui';
 
@@ -10,37 +11,53 @@ export const GamePage = () => {
 
   return (
     <div className="p-4 container flex mx-auto">
-      <div className="w-full mb-2 md:w-8/12">
-        {playing ? (
-          <>
-            <Button title="Win game" type="button" onClick={() => {}}>
-              Win game
+      <Authorize>
+        <div className="w-full mb-2 md:w-8/12 flex items-center justify-around">
+          {playing ? (
+            <>
+              <Button title="Win game" type="button" onClick={() => {}}>
+                Win game
+              </Button>
+              <Button
+                title="Lose game"
+                type="button"
+                skin="dangerInverted"
+                onClick={() => {}}
+              >
+                Lose game
+              </Button>
+              <Button
+                title="Quit"
+                type="button"
+                skin="danger"
+                onClick={() => {
+                  setGameState({
+                    playing: false,
+                  });
+                }}
+              >
+                Quit
+              </Button>
+            </>
+          ) : (
+            <Button
+              title="Start Game"
+              type="button"
+              onClick={() => {
+                setGameState({
+                  playing: true,
+                });
+              }}
+            >
+              Start Game
             </Button>
-            <Button title="Lose game" type="button" onClick={() => {}}>
-              Lose game
-            </Button>
-            <Button title="Quit" type="button" onClick={() => {}}>
-              Quit
-            </Button>
-          </>
-        ) : (
-          <Button
-            title="Start Game"
-            type="button"
-            onClick={() => {
-              setGameState({
-                playing: true,
-              });
-            }}
-          >
-            Start Game
-          </Button>
-        )}
-      </div>
+          )}
+        </div>
 
-      <div className="w-full md:w-4/12 flex flex-col items-center justify-center">
-        <Creature></Creature>
-      </div>
+        <div className="w-full md:w-4/12 flex flex-col items-center justify-center">
+          <Creature></Creature>
+        </div>
+      </Authorize>
     </div>
   );
 };
