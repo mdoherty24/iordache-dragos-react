dca9b02826f1d116a6be600a4bf8214dec971d78
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-12-02 21:36:16 +0200

    9:36

--- generic-game/src/pages/RanksPage.jsx
+++ generic-game/src/pages/RanksPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getUsers } from '../actions/creators/auth';
+import { Users } from '../components/profile';
 
 export const RanksPage = () => {
   const dispatch = useDispatch();
@@ -9,7 +10,17 @@ export const RanksPage = () => {
     dispatch(getUsers());
   }, [dispatch]);
 
-  return 'ranks';
+  return (
+    <div className="p-4 container mx-auto">
+      <header>
+        <h1 className="text-3xl bold">Ranks</h1>
+      </header>
+
+      <section className="mt-8">
+        <Users></Users>
+      </section>
+    </div>
+  );
 };
 
 export default RanksPage;
