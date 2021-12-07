990b1ae5066c10f79bfc1af48bb67a6f16f19816
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-12-07 21:33:24 +0200

    933

--- generic-next/pages/_app.jsx
+++ generic-next/pages/_app.jsx
@@ -1,7 +1,26 @@
+import { useStore } from '../store';
+import { Provider as ReduxProvider } from 'react-redux';
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const store = useStore(pageProps.initalReduxState);
+
+  return (
+    <ReduxProvider store={store}>
+      <Component {...pageProps} />
+    </ReduxProvider>
+  );
 }
 
 export default MyApp;
+
+export const getServerSideProps = async () => {
+  let pageProps = {};
+
+  // feed redux state on the backend
+  pageProps.initalReduxState = {};
+
+  return {
+    pageProps,
+  };
+};
